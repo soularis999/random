d9f1d292809750b3607fee2f6856e36ad26aca42
Dmitriy Khomitskiy <dkhomitskiy@conversantmedia.com>
2017-10-10 07:39:20 -0500

    small cleanup and adding the nio checks

--- interview_prep/random_gen.py
+++ interview_prep/random_gen.py
@@ -1,32 +1,13 @@
-import random
-
-
-def random_gen(seed, num):
+def random_gen(seed, length):
     """
-    The function generates the the random number using combination of smaller random numbers - has to be uniformly
-    distributed
-    :param seed: number of bytes the seed should be. has to be 1<= seed <= max
-    :param max: how many bytes the new number has to be - should be mod 2
-    :return: new number
+    Given the seed and the length of the number the method needs to simulate how it can create the uniformly distributed
+    number given the randomly generated numbers from 0 through seed.
+    :param seed: Number in range of 2 <= seed <= length - seed cannot be larger than the number that is needs to produce
+    Number has to be mod 2
+    :param length: length in bytes of new number
+    :return: number of length
     """
+    if seed < 2 or length < seed:
+        raise TypeError("Error with seed %s and length %s" % (seed, length))
 
-    if 0 > seed or num < seed:
-        raise TypeError("Seed and number are not in range 1 <= seed <= num: seed %s, num %s" % (seed, num))
-
-    result = 0
-    while num != 0:
-        upper = 2 ** seed - 1
-        result = (result << seed) + random.randint(0, upper)
-        num = num / seed
-    return result
-
-
-if __name__ == "__main__":
-    seed = 2
-    max = 8  # 2^8 = 256
-    f = file("random_gen.csv", "w")
-    with f:
-        f.write("index, number(%s for 2^%s = %s)\n" % (seed, max, 2 ** max))
-        for i in range(1, 50000):
-            a = random_gen(seed, max)
-            f.write("%s,%s,\n" % (i, a))
+    
