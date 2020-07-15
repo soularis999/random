2fb3e72ea6536b0f7372a1cd2c789e8636814d4f
Dmitriy Khomitskiy <dmitriy@khomitskiy.com>
2020-07-15 16:23:19 -0500

    removing clojur udemi code as there was not much useful stuff. adding only readme

--- clojure/udemi/eddy1/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to eddy1
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- clojure/udemi/eddy1/test/eddy1/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns eddy1.core-test
-  (:require [clojure.test :refer :all]
-            [eddy1.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
